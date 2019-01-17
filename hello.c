fa9c74d173b35f6cd23c694f93d4b14a6b20a6c3
Matthew Mai <matthew.mai1998@yahoo.com>
2019-01-17 12:54:50 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Matthew Mai
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Matthew!\n");
+
+  return 0;
+}
